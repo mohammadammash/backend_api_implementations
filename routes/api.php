386f09ffb2ce55f8bdc64d1e362adde2da7582e8
mohammadammash <mohammadammash31@gmail.com>
2022-09-30 14:15:40 +0300

    /routes/api.php: Adding get route to split the number given as paramter(variable) doing basic job for now

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChangeController;
 
-Route::get('/sort_string/{string}', [ChangeController::class,'sortString']);
+Route::get('/sort_string/{string}', [ChangeController::class, 'sortString']);
+Route::get('/split_number/{number}', [ChangeController::class, 'splitNumber']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
