ab4c2049ab2c0363bf6ad1f05078704efd0f045a
mohammadammash <mohammadammash31@gmail.com>
2022-09-30 19:13:27 +0300

    app/http/controllers/ChangeController.php: Fixing a bug in translateToBinary() to read and show points between numbers porperly without skipping them

--- app/Http/Controllers/ChangeController.php
+++ app/Http/Controllers/ChangeController.php
@@ -37,10 +37,10 @@ class ChangeController extends Controller
 
             if (is_numeric($char)) {
                 $temp_nb = '';
-                while (is_numeric($char) and $i < $str_length) {
-                    $char = $str[$i];
+                while ($i < $str_length and is_numeric($char)) {
                     $temp_nb = $temp_nb . $char;
                     $i += 1;
+                    if($i < $str_length)$char = $str[$i];
                 }
                 $res = $res . decbin($temp_nb);
             } else {
@@ -61,7 +61,7 @@ class ChangeController extends Controller
         // get the total count of numbers
         $num_length = count($numbers);
         //validating
-        if($num_length === 0) return $res;
+        if ($num_length === 0) return $res;
         else if ($num_length === 1) return $numbers[0];
         else $res = $numbers[0];
 
@@ -78,10 +78,10 @@ class ChangeController extends Controller
                 case '*':
                     $res *= $num;
                     break;
-                // in case of '/' it would be considered  TRAILING SLASH to indicate a directory
-                // case '/':
-                //     $res /= $num;
-                //     break;
+                    // in case of '/' it would be considered  TRAILING SLASH to indicate a directory
+                    // case '/':
+                    //     $res /= $num;
+                    //     break;
             }
         }
 
