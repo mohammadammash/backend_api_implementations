431cd6d0dfb8c59038b665f3b31c413184a82433
mohammadammash <mohammadammash31@gmail.com>
2022-09-30 14:19:55 +0300

    /routes/api.php: Adding get route to receive an expression as paramter(variable), doing basic job for now

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ChangeController;
 Route::get('/sort_string/{string}', [ChangeController::class, 'sortString']);
 Route:: get('/split_number/{number}', [ChangeController::class, 'splitNumber']);
 Route:: get('/to_binary/{str}', [ChangeController::class, 'translateToBinary']);
-Route::get('/split_number/{number}', [ChangeController::class, 'splitNumber']);
+Route::get('/calc_exp/{exp}', [ChangeController::class, 'calculateExpression']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
